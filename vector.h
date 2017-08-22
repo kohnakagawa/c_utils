e4e07da1d905b14a386006a29dad73495f2548d8
kohnakagawa <tsunekou1019@gmail.com>
2017-08-22 21:35:51 +0900

    一部のpointerをconstに

--- vector.h
+++ vector.h
@@ -7,6 +7,7 @@
 
 #define VECTOR_TYPE(T) CONCAT(vector_, T)
 #define VECTOR_REF(T) CONCAT(vectorref_, T)
+#define VECTOR_REF_CONST(T) CONCAT(vectorrefc_, T)
 #define VECTOR_TYPE_DUMMY(T) CONCAT(vector_t_, T)
 
 #define VECTOR_METHOD(T, f) CONCAT(CONCAT(VECTOR_TYPE(T), _), f)
@@ -17,7 +18,8 @@
 #define DECL_VECTOR_TYPE(T)                           \
   struct VECTOR_TYPE_DUMMY(T);                        \
   typedef struct VECTOR_TYPE_DUMMY(T) VECTOR_TYPE(T); \
-  typedef struct VECTOR_TYPE_DUMMY(T)* VECTOR_REF(T);
+  typedef struct VECTOR_TYPE_DUMMY(T)* VECTOR_REF(T); \
+  typedef const struct VECTOR_TYPE_DUMMY(T)* VECTOR_REF_CONST(T);
 
 #define DEFINE_VECTOR_TYPE(T)                   \
   DECL_VECTOR_TYPE(T)                           \
@@ -33,7 +35,7 @@
   DECL_VECTOR_NEW(T) {                                                  \
     VECTOR_REF(T) self = (VECTOR_REF(T)) xmalloc(sizeof(VECTOR_TYPE(T))); \
     self->capacity = INIT_MAX_DATA_SIZE;                                \
-    self->data = (T*) xmalloc(self->capacity * sizeof(T));               \
+    self->data = (T*) xmalloc(self->capacity * sizeof(T));              \
     self->size = 0;                                                     \
     return self;                                                        \
   }
@@ -72,14 +74,14 @@
     self->size = 0;                          \
   }
 
-#define DECL_VECTOR_AT(T) T VECTOR_METHOD(T, at)(VECTOR_REF(T) self, const size_t idx)
+#define DECL_VECTOR_AT(T) T VECTOR_METHOD(T, at)(VECTOR_REF_CONST(T) self, const size_t idx)
 #define DEFINE_VECTOR_AT(T)                                             \
   DECL_VECTOR_AT(T) {                                                   \
     RANGE_CHECK(self, idx);                                             \
     return self->data[idx];                                             \
   }
 
-#define DECL_VECTOR_AT_NOCHECK(T) T VECTOR_METHOD(T, at_nocheck)(VECTOR_REF(T) self, const size_t idx)
+#define DECL_VECTOR_AT_NOCHECK(T) T VECTOR_METHOD(T, at_nocheck)(VECTOR_REF_CONST(T) self, const size_t idx)
 #define DEFINE_VECTOR_AT_NOCHECK(T)                                   \
   DECL_VECTOR_AT_NOCHECK(T) {                                         \
     return self->data[idx];                                           \
@@ -98,13 +100,13 @@
     self->data[idx] = val;                              \
   }
 
-#define DECL_VECTOR_FRONT(T) T VECTOR_METHOD(T, front)(VECTOR_REF(T) self)
+#define DECL_VECTOR_FRONT(T) T VECTOR_METHOD(T, front)(VECTOR_REF_CONST(T) self)
 #define DEFINE_VECTOR_FRONT(T)                          \
   DECL_VECTOR_FRONT(T) {                                \
     return VECTOR_METHOD(T, at_nocheck)(self, 0);               \
   }
 
-#define DECL_VECTOR_BACK(T) T VECTOR_METHOD(T, back)(VECTOR_REF(T) self)
+#define DECL_VECTOR_BACK(T) T VECTOR_METHOD(T, back)(VECTOR_REF_CONST(T) self)
 #define DEFINE_VECTOR_BACK(T)                           \
   DECL_VECTOR_BACK(T) {                                 \
     return VECTOR_METHOD(T, at_nocheck)(self, self->size - 1);  \
@@ -130,19 +132,19 @@
     for (size_t i = 0; i < self->size; i++) self->data[i] = val;  \
   }
 
-#define DECL_VECTOR_SIZE(T) size_t VECTOR_METHOD(T, size)(VECTOR_REF(T) self)
+#define DECL_VECTOR_SIZE(T) size_t VECTOR_METHOD(T, size)(VECTOR_REF_CONST(T) self)
 #define DEFINE_VECTOR_SIZE(T)                 \
   DECL_VECTOR_SIZE(T) {                       \
     return self->size;                        \
   }
 
-#define DECL_VECTOR_CAPACITY(T) size_t VECTOR_METHOD(T, capacity)(VECTOR_REF(T) self)
+#define DECL_VECTOR_CAPACITY(T) size_t VECTOR_METHOD(T, capacity)(VECTOR_REF_CONST(T) self)
 #define DEFINE_VECTOR_CAPACITY(T)                   \
   DECL_VECTOR_CAPACITY(T) {                         \
     return self->capacity;                          \
   }
 
-#define DECL_VECTOR_EMPTY(T) bool VECTOR_METHOD(T, empty)(VECTOR_REF(T) self)
+#define DECL_VECTOR_EMPTY(T) bool VECTOR_METHOD(T, empty)(VECTOR_REF_CONST(T) self)
 #define DEFINE_VECTOR_EMPTY(T)               \
   DECL_VECTOR_EMPTY(T) {                     \
     return (bool)(0 == self->size);          \
